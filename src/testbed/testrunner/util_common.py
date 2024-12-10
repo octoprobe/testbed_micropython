121053a1e3b776a4fea126d83babc2f3e91dd16a
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2024-12-11 10:32:20 +1100

    mip install

--- src/testbed/testrunner/util_common.py
+++ src/testbed/testrunner/util_common.py
@@ -3,12 +3,54 @@ These methods may be used by
 testbed_showcase and testbed_micropython
 """
 
+from __future__ import annotations
+
 import dataclasses
 import pathlib
+import sys
+import typing
 
+from octoprobe.lib_tentacle import Tentacle
+from octoprobe.util_subprocess import subprocess_run
 from octoprobe.util_vscode_un_monkey_patch import un_monkey_patch
 
 from testbed.util_firmware_mpbuild import CachedGitRepo
+
+if typing.TYPE_CHECKING:
+    from testbed.testcollection.testrun_specs import TestArgs
+
+
+def mip_install(
+    testargs: TestArgs, tentacle: Tentacle, port: str, mip_package: str
+) -> None:
+    assert testargs.__class__.__name__ == "TestArgs"
+    assert isinstance(tentacle, Tentacle)
+    assert isinstance(port, str)
+    assert isinstance(mip_package, str)
+    if False:
+        # Using internal mp_remote connection
+        logger.info(f"{tentacle.dut.label}: mip install {mip_package}")
+        tentacle.dut.mp_remote.mip_install_package(mip_package)
+        return
+
+    # Using external mpremote mip install
+    args = [
+        sys.executable,
+        "-m",
+        "mpremote",
+        "mip",
+        f"--target={tentacle.dut.get_tty()}",
+        "install",
+        mip_package,
+    ]
+    subprocess_run(
+        args=args,
+        cwd=testargs.git_micropython_tests / "tests",
+        logfile=testargs.testresults_directory(
+            f"mip_insall_{mip_package}.txt"
+        ).filename,
+        timeout_s=60.0,
+    )
 
 
 @dataclasses.dataclass
