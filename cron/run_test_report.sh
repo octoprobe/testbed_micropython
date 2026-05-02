2a8bffa7507514e677203b1be06121cc1dac311d
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-05-02 19:46:03 +0200

    cron/run_test_report.sh: Switch back to multiprocessing

--- cron/run_test_report.sh
+++ cron/run_test_report.sh
@@ -12,8 +12,5 @@ git reset --hard origin/main
 DIRECTORY_REPO=$(git rev-parse --show-toplevel)
 [[ -d "${DIRECTORY_REPO}/.git" ]] || { echo "ERROR: '${DIRECTORY_REPO}/.git' is not a directory"; exit 1; }
 
-# mptest test --micropython-tests="$REPO" --firmware-build="$REPO" --multiprocessing --count=2 --skip-fut=FUT_WLAN --skip-fut=FUT_BLE 
-# mptest report --testresults=./testresults --email=buhtig.hans.maerki@ergoinfo.ch --email=damien@micropython.org 
-
-mptest test --testresults=$DIRECTORY_REPO/testresults --micropython-tests="$REPO" --firmware-build="$REPO" --no-multiprocessing --count=2 --skip-fut=FUT_WLAN --skip-fut=FUT_BLE 
+mptest test --testresults=$DIRECTORY_REPO/testresults --micropython-tests="$REPO" --firmware-build="$REPO" --multiprocessing --count=3 --skip-fut=FUT_WLAN --skip-fut=FUT_BLE 
 mptest report --testresults=$DIRECTORY_REPO/testresults --email=buhtig.hans.maerki@ergoinfo.ch --email=damien@micropython.org 
