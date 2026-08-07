e4400f762e6b1a9a083322c78ec6cb4df1ec9ad9
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-08-07 07:05:37 +0200

    Add test_xfail.py 1

--- src/testbed_micropython/report_test/util_xfail.py
+++ src/testbed_micropython/report_test/util_xfail.py
@@ -1,17 +1,77 @@
+"""
+Example XFailFile:
+
+{
+    // Test "*" on testgroup
+    "*": {
+        "testgroup.py": [
+            "TESTGROUP"
+        ]
+    },
+    "RUN-TESTS_STANDARD": {
+        // Test "*" on test_name
+        "*": [
+            "PYBV11"
+        ],
+        "extmod/socket_udp_nonblock.py": [
+            "ESP32_C3_DEVKIT",
+            "ESP32_S3_DEVKIT",
+            "LOLIN_C3_MINI",
+        ],
+        "test_name.py": [
+            // Test "*" on board_variant
+            "*"
+        ]
+    },
+}
+
+Note that there may be comment (//).
+Note the CATCHALL ('*')
+
+"""
+
 from __future__ import annotations
 
 import dataclasses
 import json
 import pathlib
+import re
 
 DIRECTORY_OF_THIS_FILE = pathlib.Path(__file__).parent
 
+CATCHALL = "*"
 
-class XFailGroup(dict[str, set[str]]):
+
+class XFailBoardVariant(set[str]):
+    def match(self, board_variant: str) -> bool:
+        """
+        Test on level 'board_variant' (ESP32_C3_DEVKIT, ESP32_S3_DEVKIT)
+        """
+        for pattern in (board_variant, CATCHALL):
+            if pattern in self:
+                return True
+        return False
+
+
+class XFailGroup(dict[str, XFailBoardVariant]):
     def add(self, board_variant: str, test_name: str) -> None:
-        names = self.get(test_name, set())
+        names = self.get(test_name)
+        if names is None:
+            names = XFailBoardVariant()
         names.add(board_variant)
-        self[test_name] = names
+        self[test_name] = XFailBoardVariant(names)
+
+    def match(self, test_name: str, board_variant: str) -> bool:
+        """
+        Test on level 'test_name' (extmod/socket_udp_nonblock.py, )
+        """
+        for pattern in (test_name, CATCHALL):
+            xfail_boardvariant = self.get(pattern)
+            if xfail_boardvariant is not None:
+                matched = xfail_boardvariant.match(board_variant=board_variant)
+                if matched:
+                    return True
+        return False
 
     @property
     def to_dict(self) -> dict[str, list[str]]:
@@ -19,7 +79,7 @@ class XFailGroup(dict[str, set[str]]):
 
     @staticmethod
     def from_dict(dict_group: dict[str, list[str]]) -> XFailGroup:
-        return XFailGroup({k: set(v) for k, v in dict_group.items()})
+        return XFailGroup({k: XFailBoardVariant(v) for k, v in dict_group.items()})
 
 
 class XFailList(dict[str, XFailGroup]):
@@ -39,10 +99,28 @@ class XFailList(dict[str, XFailGroup]):
             json.dump(obj=self.to_dict, fp=f, sort_keys=True, indent=4)
 
     def match(self, testgroup: str, test_name: str, board_variant: str) -> bool:
-        try:
-            return board_variant in self[testgroup][test_name]
-        except KeyError:
+        """
+        Finds an entry matching 'testgroup', 'test_name' and 'board_variant' in the json file.
+        return True if found.
+        Also considers CATCHALL ('*').
+        """
+
+        def match() -> bool:
+            """
+            Test on level 'testgroup' (RUN-TESTS_STANDARD, RUN-TESTS_STANDARD_NATIVE)
+            """
+            for pattern in (testgroup, CATCHALL):
+                xfailgroup = self.get(pattern)
+                if xfailgroup is not None:
+                    matched = xfailgroup.match(
+                        test_name=test_name,
+                        board_variant=board_variant,
+                    )
+                    if matched:
+                        return True
             return False
+
+        return match()
 
     @staticmethod
     def from_dict(dict_report: dict[str, dict[str, list[str]]]) -> XFailList:
@@ -50,9 +128,20 @@ class XFailList(dict[str, XFailGroup]):
 
     @staticmethod
     def factory(filename: pathlib.Path) -> XFailList:
+        re_comment = re.compile(r"^(.*?)//")
+
+        def remove_comment(line: str) -> str:
+            line = line.strip()
+            match = re_comment.match(line)
+            if match:
+                return match.groups()[0]
+            return line
+
         with filename.open("r") as f:
-            dict_report = json.load(fp=f)
-            return XFailList.from_dict(dict_report)
+            lines = [remove_comment(line) for line in f.readlines()]
+        json_text = "\n".join(lines)
+        dict_report = json.loads(json_text)
+        return XFailList.from_dict(dict_report)
 
 
 @dataclasses.dataclass(slots=True)
--- tests/test_xfail.py
+++ tests/test_xfail.py
@@ -1,20 +1,64 @@
 import pathlib
 
+import pytest
+
 from testbed_micropython.report_test import util_xfail
 
 FILENAME_XFAIL = pathlib.Path(__file__).with_name("test_xfail_input.json")
 
 
-def test_xfail() -> None:
+@pytest.mark.parametrize(
+    "expected,testgroup,test_name,board_variant",
+    (
+        (
+            True,
+            "RUN-TESTS_STANDARD",
+            "extmod/socket_udp_nonblock.py",
+            "PYBV11",
+        ),
+        (
+            False,
+            "RUN-TESTS_STANDARD_NATIVE",
+            "extmod/socket_udp_nonblock.py",
+            "DUMMY",
+        ),
+        (
+            # Test '*' on testgroup
+            True,
+            "anyvalue",
+            "testgroup.py",
+            "TESTGROUP",
+        ),
+        (
+            # Test '*' on test_name
+            True,
+            "RUN-TESTS_STANDARD",
+            "anyvalue",
+            "PYBV11",
+        ),
+        (
+            # Test "*" on board_variant
+            True,
+            "RUN-TESTS_STANDARD",
+            "test_name.py",
+            "ANYVALUE",
+        ),
+    ),
+)
+def test_xfail(
+    testgroup: str,
+    test_name: str,
+    board_variant: str,
+    expected: bool,
+) -> None:
     xfail_file = util_xfail.XFailFile.factory(filename=FILENAME_XFAIL)
+    match = xfail_file.xfail_list.match
 
-    assert xfail_file.xfail_list.match(
-        testgroup="RUN-TESTS_STANDARD",
-        test_name="extmod/socket_udp_nonblock.py",
-        board_variant="PYBV11",
-    )
-    assert not xfail_file.xfail_list.match(
-        testgroup="RUN-TESTS_STANDARD_NATIVE",
-        test_name="extmod/socket_udp_nonblock.py",
-        board_variant="DUMMY",
+    assert (
+        match(
+            testgroup=testgroup,
+            test_name=test_name,
+            board_variant=board_variant,
+        )
+        == expected
     )
