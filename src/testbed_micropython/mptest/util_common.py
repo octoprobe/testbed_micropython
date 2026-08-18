2b1c372635e6754c3f5392b7c61085c52ea4a5ae
Damien George <damien@micropython.org>
2026-08-18 10:26:33 +1000

    Copy PEM certificates to device for network tests
    MicroPython now supports loading `.pem` certificate files and has a test
    for it, so copy them in addition to `.der`.
    
    Signed-off-by: Damien George <damien@micropython.org>
    (cherry picked from commit ac3aac895840df90c3c6f249459e107488ccdb72)

--- src/testbed_micropython/mptest/util_common.py
+++ src/testbed_micropython/mptest/util_common.py
@@ -120,7 +120,7 @@ def copy_certificates(dut: TentacleDut, src: pathlib.Path) -> None:
 
     dut.mp_remote.set_rtc()
 
-    for certificate in src.glob("*.der"):
+    for certificate in list(src.glob("*.der")) + list(src.glob("*.pem")):
         logger.info(f"{dut.label}: copy_certificates(): {certificate.name}")
         dut.mp_remote.cp(certificate, ":")
 
